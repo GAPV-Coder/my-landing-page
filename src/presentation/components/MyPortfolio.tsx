d9d2ffec1a4da0f66735ec5265ebda1b0f22fb3e
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2024-12-27 22:43:17 -0500

    Update translations component MyPortfolio.tsx

--- src/presentation/components/MyPortfolio.tsx
+++ src/presentation/components/MyPortfolio.tsx
@@ -22,31 +22,31 @@ interface Project {
 const projects: Project[] = [
     {
         title: 'Project 1',
-        image: '/placeholder.svg?height=400&width=600',
+        image: 'https://res.cloudinary.com/dpmpznfta/image/upload/v1735351930/samples/pexels-mo-eid-1268975-9454915_qk91nc.jpg',
         github: 'https://github.com/username/project1',
         demo: 'https://project1.demo',
     },
     {
         title: 'Project 2',
-        image: '/placeholder.svg?height=400&width=600',
+        image: 'https://res.cloudinary.com/dpmpznfta/image/upload/v1735351929/samples/pexels-rostislav-6807303_mhwidu.jpg',
         github: 'https://github.com/username/project2',
         demo: 'https://project2.demo',
     },
     {
         title: 'Project 3',
-        image: '/placeholder.svg?height=400&width=600',
+        image: 'https://res.cloudinary.com/dpmpznfta/image/upload/v1735351928/samples/pexels-mo-eid-1268975-9888656_ybztyb.jpg',
         github: 'https://github.com/username/project3',
         demo: 'https://project3.demo',
     },
     {
         title: 'Project 4',
-        image: '/placeholder.svg?height=400&width=600',
+        image: 'https://res.cloudinary.com/dpmpznfta/image/upload/v1735351928/samples/pexels-steve-28858124_lzrs33.jpg',
         github: 'https://github.com/username/project4',
         demo: 'https://project4.demo',
     },
     {
         title: 'Project 5',
-        image: '/placeholder.svg?height=400&width=600',
+        image: 'https://res.cloudinary.com/dpmpznfta/image/upload/v1735351927/samples/pexels-googledeepmind-18069694_mmahqi.jpg',
         github: 'https://github.com/username/project5',
         demo: 'https://project5.demo',
     },
@@ -57,11 +57,18 @@ const MyPortfolio = () => {
     const [api, setApi] = useState<any>();
     const [isPaused, setIsPaused] = useState(false);
 
+    const extendedProjects = [...projects, ...projects];
+
     useEffect(() => {
         if (!api || isPaused) return;
 
         const interval = setInterval(() => {
-            api.scrollNext();
+            if (api.canScrollNext()) {
+                api.scrollNext();
+            } else {
+                // Reinicia al principio sin interrupción
+                api.scrollTo(0);
+            }
         }, 3000);
 
         return () => clearInterval(interval);
@@ -78,30 +85,21 @@ const MyPortfolio = () => {
                 <div className="space-y-12 lg:w-3/4">
                     <div className="space-y-4">
                         <h2 className="text-titleColor text-lg oswald-medium tracking-wide">
-                            DESARROLLADOR FULL STACK, FRONTEND Y BACKEND
+                            {t('DESARROLLADOR FULL STACK, FRONTEND Y BACKEND')}
                         </h2>
                         <h1 className="text-4xl md:text-5xl oswald-bold">
-                            MIS PORTAFOLIOS RECIENTES
+                            {t('MIS PORTAFOLIOS RECIENTES')}
                         </h1>
                     </div>
 
                     <div className="space-y-4 bg-white/10 backdrop-blur-sm border-white/20 hover:border-white/40 transition-colors duration-300 p-3">
                         <h3 className="text-2xl text-titleColor oswald-semiBold">
-                            PROYECTOS DESTACADOS
+                            {t('PROYECTOS DESTACADOS')}
                         </h3>
                         <p className="text-white oswald-regular">
-                            He desarrollado proyectos personales que reflejan
-                            mis habilidades técnicas como desarrollador Full
-                            Stack, destacando el consumo de API's y la
-                            integración con pasarelas de pago. Entre mis logros
-                            se encuentran el desarrollo de plataformas SaaS
-                            funcionales y escalables, billeteras virtuales con
-                            alta seguridad y aplicaciones web enfocadas en
-                            experiencias de usuario optimizadas. Estos proyectos
-                            demuestran mi capacidad para implementar soluciones
-                            completas, desde el backend hasta el frontend,
-                            utilizando tecnologías modernas y metodologías de
-                            desarrollo efectivas.
+                            {t(
+                                `He desarrollado proyectos personales que reflejan mis habilidades técnicas como desarrollador Full Stack, destacando el consumo de API's y la integración con pasarelas de pago. Entre mis logros se encuentran el desarrollo de plataformas SaaS funcionales y escalables, billeteras virtuales con alta seguridad y aplicaciones web enfocadas en experiencias de usuario optimizadas. Estos proyectos demuestran mi capacidad para implementar soluciones completas, desde el backend hasta el frontend, utilizando tecnologías modernas y metodologías de desarrollo efectivas.`,
+                            )}
                         </p>
                     </div>
                 </div>
@@ -113,9 +111,10 @@ const MyPortfolio = () => {
                         className="w-full"
                         opts={{
                             align: 'start',
+                            loop: true,
                         }}>
                         <CarouselContent>
-                            {projects.map((project, index) => (
+                            {extendedProjects.map((project, index) => (
                                 <CarouselItem
                                     key={index}
                                     className="sm:basis-full md:basis-1/2 lg:basis-1/2">
@@ -128,7 +127,7 @@ const MyPortfolio = () => {
                                                 <img
                                                     src={project.image}
                                                     alt={project.title}
-                                                    className="w-full h-[400px] object-cover"
+                                                    className="w-full h-[400px] object-cover rounded-t-xl"
                                                 />
                                                 <div className="p-4">
                                                     <div className="flex items-center justify-between">
